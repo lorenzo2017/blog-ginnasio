8cc1151b7212e571dc4651c00f3292ca1976985e
lorenzo <lorenzo.ragni2017@gmail.com>
2015-06-01 21:59:41 +0200

    blog

---
+++ ckeditor/build-config.js
@@ -0,0 +1,106 @@
+﻿/**
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+/**
+ * This file was added automatically by CKEditor builder.
+ * You may re-use it at any time to build CKEditor again.
+ *
+ * If you would like to build CKEditor online again
+ * (for example to upgrade), visit one the following links:
+ *
+ * (1) http://ckeditor.com/builder
+ *     Visit online builder to build CKEditor from scratch.
+ *
+ * (2) http://ckeditor.com/builder/ece56231441667e4a77b04a123c5b492
+ *     Visit online builder to build CKEditor, starting with the same setup as before.
+ *
+ * (3) http://ckeditor.com/builder/download/ece56231441667e4a77b04a123c5b492
+ *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
+ *
+ * NOTE:
+ *    This file is not used by CKEditor, you may remove it.
+ *    Changing this file will not change your CKEditor configuration.
+ */
+
+var CKBUILDER_CONFIG = {
+	skin: 'bootstrapck',
+	preset: 'standard',
+	ignore: [
+		'.bender',
+		'bender.js',
+		'bender-err.log',
+		'bender-out.log',
+		'dev',
+		'.DS_Store',
+		'.editorconfig',
+		'.gitattributes',
+		'.gitignore',
+		'gruntfile.js',
+		'.idea',
+		'.jscsrc',
+		'.jshintignore',
+		'.jshintrc',
+		'.mailmap',
+		'node_modules',
+		'package.json',
+		'README.md',
+		'tests'
+	],
+	plugins : {
+		'SimpleLink' : 1,
+		'about' : 1,
+		'autogrow' : 1,
+		'backgrounds' : 1,
+		'basicstyles' : 1,
+		'blockquote' : 1,
+		'ckeditor-gwf-plugin' : 1,
+		'clipboard' : 1,
+		'contextmenu' : 1,
+		'enterkey' : 1,
+		'entities' : 1,
+		'filebrowser' : 1,
+		'flash' : 1,
+		'floatingspace' : 1,
+		'font' : 1,
+		'fontawesome' : 1,
+		'format' : 1,
+		'horizontalrule' : 1,
+		'image' : 1,
+		'image2' : 1,
+		'imagepaste' : 1,
+		'imageresize' : 1,
+		'imgbrowse' : 1,
+		'indentlist' : 1,
+		'link' : 1,
+		'list' : 1,
+		'magicline' : 1,
+		'maximize' : 1,
+		'oembed' : 1,
+		'pastefromword' : 1,
+		'pastetext' : 1,
+		'preview' : 1,
+		'removeformat' : 1,
+		'resize' : 1,
+		'scayt' : 1,
+		'showborders' : 1,
+		'sourcearea' : 1,
+		'specialchar' : 1,
+		'stylescombo' : 1,
+		'tab' : 1,
+		'table' : 1,
+		'tableresize' : 1,
+		'tabletools' : 1,
+		'tliyoutube' : 1,
+		'toolbar' : 1,
+		'undo' : 1,
+		'wordcount' : 1,
+		'wsc' : 1,
+		'wysiwygarea' : 1
+	},
+	languages : {
+		'en' : 1,
+		'it' : 1
+	}
+};
